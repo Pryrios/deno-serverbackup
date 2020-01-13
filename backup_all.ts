5929c0d7dda60629b4908488df587618a1d3e1a8
Daniel Ramirez <pryrios@gmail.com>
2020-01-13 16:14:10 +0000

    Initial concept code

---
+++ backup_all.ts
@@ -0,0 +1,49 @@
+interface DatabaseConfig{
+  database: string;
+  username: string;
+  password: string;
+}
+
+async function backupDatabase(dbConfig: DatabaseConfig, backupDir: string) {
+  console.log("Backup database: " + dbConfig.database);
+  const now = new Date();
+  let filename = backupDir + dbConfig.database + "_dump_" + now.getFullYear() + now.getMonth() + now.getDay() + now.getHours() + now.getMinutes() + ".sql";
+  console.log(filename);
+  const p = Deno.run({
+    args: [
+      "mysqldump",
+      "-u",
+      dbConfig.username,
+      "-p" + dbConfig.password,
+      dbConfig.database
+    ],
+    stdout: "piped",
+    stderr: "piped"
+  });
+
+  const { code } = await p.status();
+  if (code === 0) {
+    const rawOutput = await p.output();
+    const file = await Deno.open(filename, "a+");
+    await file.write(rawOutput);
+    file.close();
+  } else {
+    const rawError = await p.stderrOutput();
+    const errorString = new TextDecoder().decode(rawError);
+    return errorString;
+  }
+
+  return dbConfig.database;
+}
+
+async function runBackups() {
+  const content = await Deno.readFile("config.json");
+  const contentStr = new TextDecoder().decode(content);
+  const config = JSON.parse(contentStr);
+
+  return Promise.all(config.databases.map(element => backupDatabase(element, config.backupDir)));
+}
+
+runBackups().then(data => {
+  console.log(data)
+});
